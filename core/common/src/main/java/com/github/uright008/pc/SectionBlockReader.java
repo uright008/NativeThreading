391f7ffbd251425ff71d003ce2f22d4b6987a5d3
uright008 <admin@uright.icu>
2026-06-20 13:17:23 +0800

    refactor: SectionBlockReader uses reflection to call PalettedContainer.get(int)
    Removes the @Invoker mixin approach (PalettedContainerGetInvoker) because
    Mixin's @Invoker on an interface doesn't make the target class implement
    the interface - the ClassCastException at runtime was unavoidable.
    
    Instead, use java.lang.reflect.Method.setAccessible(true) on the protected
    PalettedContainer.get(int) method. Clean, simple, no agent/Javassist needed.

--- core/common/src/main/java/com/github/uright008/pc/SectionBlockReader.java
+++ core/common/src/main/java/com/github/uright008/pc/SectionBlockReader.java
@@ -1,25 +1,41 @@
 package com.github.uright008.pc;
 
-import com.github.uright008.pc.mixin.PalettedContainerGetInvoker;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.chunk.PalettedContainer;
+import java.lang.reflect.Method;
 
 public final class SectionBlockReader {
 
-    private final PalettedContainerGetInvoker invoker;
+    private final PalettedContainer<BlockState> container;
 
-    private SectionBlockReader(PalettedContainerGetInvoker invoker) {
-        this.invoker = invoker;
+    private SectionBlockReader(PalettedContainer<BlockState> container) {
+        this.container = container;
+    }
+
+    private static final Method GET;
+
+    static {
+        try {
+            GET = PalettedContainer.class.getDeclaredMethod("get", int.class);
+            GET.setAccessible(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static SectionBlockReader of(LevelChunkSection section) {
         PalettedContainer<BlockState> c = section.getStates();
-        return new SectionBlockReader((PalettedContainerGetInvoker)(Object)c);
+        return new SectionBlockReader(c);
     }
 
+    @SuppressWarnings("unchecked")
     public BlockState get(int x, int y, int z) {
         int idx = (y & 15) << 8 | (z & 15) << 4 | (x & 15);
-        return invoker.invokeGet(idx);
+        try {
+            return (BlockState) GET.invoke(container, idx);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
