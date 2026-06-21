51b939457dcbb4126e19214c55740406dea29386
uright008 <admin@uright.icu>
2026-06-21 16:09:29 +0800

    chore(build): add Java 25 toolchain to fabric subproject

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -83,6 +83,9 @@ java {
     withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_25
     targetCompatibility = JavaVersion.VERSION_25
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
 }
 
 tasks.named("sourcesJar") {
